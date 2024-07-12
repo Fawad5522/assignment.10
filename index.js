12638f0638a758176b6c73a5b5148885410efa21
Fawad5522 <fadeejaan846@gmail.com>
2024-07-12 14:23:09 +0500

    Pushing Programs(task)

---
+++ index.js
@@ -0,0 +1,64 @@
+// 6 . Smallest No in Array :
+
+// var num = []
+// for (var i = 0; i < 5; i++) {
+//    var numbers = +prompt("Enter 5 Number")
+//    num.push(numbers)
+// }
+// var smallest = num[0]
+// for (i = 1; i < 5; i++) {
+//     if (num[i] < smallest) {
+//         smallest = num[i]
+//     }
+// }
+// console.log("smallest no : " +smallest)
+
+
+// 7. celius to Fahrenheit
+// Formula :  F = °C × (9/5) + 32. 
+
+// var c ,f 
+//  c = +prompt("Enter Celsius to convert it to fahrenheit")
+// f = c * (9/5) + 32
+// console.log("Fahrenheit : " +f)
+
+
+// 8. fahrenheit to celius
+// Formula : F = °C × (9/5) + 32.
+
+// var c, f;
+// f = +prompt("Enter Fahrenheit value to convert it to Celsius:");
+// c = ((f - 32) * (5 / 9));
+// console.log("Celsius : " +c);
+
+
+// 9. REVERSE AN ARRAY
+
+// var num = []
+// for (var i = 0; i < 5; i++) {
+//     var numbers = +prompt("Enter 5 number to reverse")
+//     num.push(numbers)
+//     console.log("array[" + i + "] = " + numbers); 
+// }
+// for (var i = 4; i >= 0; i--) {
+//     console.log("array[" + i + "] = " + num[i]);
+//   }
+
+
+// 10. CALCULATE AVERAGE OF AN ARRAY
+
+// var num = []
+// var sum = 0
+// for (var i = 0; i < 5; i++) {
+//    var numbers = +prompt("Enter 5 Number")
+//    num.push(numbers)
+//    console.log(numbers)
+//    sum = sum + num[i]
+// }
+//   console.log("Sum Of Array : " +sum)
+//   var average = sum / i;
+//   console.log("Average : " +average)
+
+
+
+       
